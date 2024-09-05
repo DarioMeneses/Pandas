4dc45f483623f714deeec3d4e2d6afbf565c56a9
Dario Meneses <dariomeneses1995@gmail.com>
2024-09-04 21:11:00 -0500

    Primera sesion Pandas

---
+++ Sesion_1/Series_Dataframes.py
@@ -0,0 +1,20 @@
+#Vamos a importar la biblioteca de pandas
+
+import pandas as pd
+psg_players = pd.Series(
+    ["Navas", "Mbappe", "Neymar", "Messi"],
+    index=[1,7,10,30]
+)
+print(psg_players)
+psg_dict = {1: "Navas", 7: "Mbappe", 10: "Neymar", 30: "Messi"}
+psg_players_dict = pd.Series(psg_dict)
+print(psg_players_dict)
+
+# Diccionario con datos de jugadores
+dict = {"Jugador": ["Navas", "Mbappe", "Neymar", "Messi"],
+        "Altura": [183.0, 170.0, 165.0],
+        "Goles": [2, 200, 150, 200]    
+}
+
+df = pd.DataFrame(dict, index=[1,7,10,30])
+print(df)
